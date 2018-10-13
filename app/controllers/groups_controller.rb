4527d95d1a5f0c7de11ad2a1392f9e5f4bc4720d
lily_bell514 <lilybell.5555@gmail.com>
2018-10-13 14:29:33 +0900

    レビュー指摘箇所の訂正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,7 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
 
   def index
-    @group = Group.all.includes(:user)
+    @groups = Group.all.includes(:user)
   end
 
   def new
